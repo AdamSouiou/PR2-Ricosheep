12a7f5a4f9ab7b2f5a447d56cac16c6aed8c00e2
ABDALLAH AMAL <amal.abdallah@localhost.localdomain>
2022-04-07 15:04:41 +0200

    Preparation des classes et commandes.

--- mouton.py
+++ mouton.py
@@ -1,11 +1,35 @@
 from dataclasses import dataclass
 from typing import List, Tuple
+from plateau import Case
+
+import graphiques
+
 
 class Mouton:
+    x: int
+    y: int
+    img_neutre: object
+    img_herbe: object
+
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+        self.img_neutre = "ntm"
+        self.img_herbe = "ntp"
     
-    def deplace_mouton(direction: str, plateau: List[List[]]):
-        pass
+    def deplace_mouton(self, direction: str, plateau: List[List[Case]]):
+        if direction == "Up":
+            print("HAUT")
+
+        if direction == "Left":
+            print("GAUCHE")
+
+        if direction == "Down":
+            print("BAS")
+
+        if direction == "Right":
+            print("DROITE")
+    
+    def is_eating(self, plateau: List[List]):
+        return plateau[self.y][self.x] == "G"
 
