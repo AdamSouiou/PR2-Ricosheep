e931247cc9c6f2a211a2299866c072514d6488e1
Coding Supremacy <codingsupremacy@gmail.com>
2022-05-20 16:24:00 +0200

    Ajout du solveur en largeur, enfin

--- solveur.py
+++ solveur.py
@@ -2,6 +2,7 @@ from typing import List, Tuple, Set
 from plateau import Plateau
 import fltk
 from time import sleep
+import collections
 
 DIRECTIONS = ("Up", "Right", "Down", "Left")
 
@@ -41,6 +42,37 @@ def profondeur(plateau,
         restore(plateau.troupeau, positions_initiales)
 
     return None, visite
+
+
+def largeur(plateau) -> List[str]:
+
+    directions = DIRECTIONS
+    visite = set()
+    parcours = collections.deque()
+    parcours.append((tri_copy(plateau.troupeau), []))
+    
+    while parcours:
+        positions, chemin = parcours.popleft()
+        restore(plateau.troupeau, positions)
+
+        if plateau.isGagne():
+            return chemin
+
+        if positions in visite:
+            continue
+
+        visite.add(positions)
+        
+        if chemin:
+            if   chemin[-1] in ("Up", "Down"): directions = ("Left", "Right")
+            elif chemin[-1] in ("Right", "Left"): directions = ("Up", "Down")
+
+        for direction in directions:
+            plateau.deplace_moutons(direction, solveur=True)
+            parcours.append((tri_copy(plateau.troupeau), chemin + [direction]))
+            restore(plateau.troupeau, positions)
+    
+    return None
 
 
 def tri_copy(troupeau):
